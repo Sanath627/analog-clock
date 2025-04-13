653a53061fe79e8f2557419c3a9708e62e20754e
Sanath D U <sanathdu@Sanaths-MacBook-Air.local>
2025-04-13 23:26:40 +0530

    change

--- src/AnalogClock.jsx
+++ src/AnalogClock.jsx
@@ -4,25 +4,25 @@ const AnalogClock = () => {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    const tictic = setInterval(() => {
       setTime(new Date());}, 1000);
-    return () => clearInterval(interval);
+    return () => clearInterval(tictic);
   }, []);
 
   const hours = time.getHours() % 12;
   const min = time.getMinutes();
   const sec = time.getSeconds();
 
-  const hourDeg = (hours * 30) + (min / 2); 
-  const minuteDeg = min * 6; 
-  const secondDeg = sec * 6; 
+  const hour = (hours * 30) + (min / 2); 
+  const minute = min * 6; 
+  const second = sec * 6; 
 
   return (
     <div className="clock">
       <div className="clock-face">
-        <div className="hour-hand" style={{ transform: `rotate(${hourDeg}deg)` }}></div>
-        <div className="minute-hand" style={{ transform: `rotate(${minuteDeg}deg)` }}></div>
-        <div className="second-hand" style={{ transform: `rotate(${secondDeg}deg)` }}></div>
+        <div className="hour-hand" style={{ transform: `rotate(${hour}deg)` }}></div>
+        <div className="minute-hand" style={{ transform: `rotate(${minute}deg)` }}></div>
+        <div className="second-hand" style={{ transform: `rotate(${second}deg)` }}></div>
       </div>
     </div>
   );
